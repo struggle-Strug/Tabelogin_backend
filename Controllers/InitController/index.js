c99909aa158cec3efb8def5e028ce967c6042ee3
struggle-Strug <strugstruggle0430@gmail.com>
2025-04-21 19:05:17 -0700

    fix database init function

--- Controllers/InitController/index.js
+++ Controllers/InitController/index.js
@@ -1,73 +1,83 @@
 module.exports = async (db) => {
   try {
     await db.query(`
-          CREATE TABLE IF NOT EXISTS users (
-              id INT PRIMARY KEY AUTO_INCREMENT,
-              name VARCHAR(255) DEFAULT NULL,
-              email VARCHAR(255) UNIQUE NOT NULL,
-              password VARCHAR(255) NOT NULL,
-              sex VARCHAR(255) DEFAULT NULL,
-              birthday VARCHAR(255) DEFAULT NULL,
-              introduce VARCHAR(255) DEFAULT NULL,
-              image VARCHAR(255) DEFAULT NULL,
-              updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-              created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-          );`);
+      CREATE TABLE IF NOT EXISTS users (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(255) DEFAULT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        sex VARCHAR(255) DEFAULT NULL,
+        birthday VARCHAR(255) DEFAULT NULL,
+        introduce VARCHAR(255) DEFAULT NULL,
+        image VARCHAR(255) DEFAULT NULL,
+        updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
 
     await db.query(`
-          CREATE TABLE IF NOT EXISTS shops (
-              id INT PRIMARY KEY AUTO_INCREMENT,
-              user_id INT NOT NULL,
-              date VARCHAR(255) DEFAULT NULL,
-              start_time VARCHAR(255) DEFAULT NULL,
-              end_time VARCHAR(255) DEFAULT NULL,
-              content VARCHAR(255) DEFAULT NULL,
-              remarks VARCHAR(255) DEFAULT NULL,
-              updated DATE DEFAULT (CURRENT_DATE),
-              created_at DATE DEFAULT (CURRENT_DATE),
-              FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-          );`);
+      CREATE TABLE IF NOT EXISTS shops (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        address VARCHAR(255) DEFAULT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        introduce VARCHAR(255) DEFAULT NULL,
+        name VARCHAR(255) DEFAULT NULL,
+        password VARCHAR(255) NOT NULL,
+        updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
 
     await db.query(`
-          CREATE TABLE IF NOT EXISTS schedules (
-              id INT PRIMARY KEY AUTO_INCREMENT,
-              user_id INT NOT NULL,
-              content VARCHAR(255) DEFAULT NULL,
-              updated DATE DEFAULT (CURRENT_DATE),
-              created_at DATE DEFAULT (CURRENT_DATE),
-              FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-          );`);
+      CREATE TABLE IF NOT EXISTS contents (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        user_id INT NOT NULL,
+        title VARCHAR(255) DEFAULT NULL,
+        content VARCHAR(255) DEFAULT NULL,
+        updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+      );
+    `);
 
     await db.query(`
-          CREATE TABLE IF NOT EXISTS messages (
-              id INT PRIMARY KEY AUTO_INCREMENT,
-              user_id INT NOT NULL,
-              is_ai BOOLEAN DEFAULT 0,
-              message VARCHAR(255) DEFAULT NULL,
-              updated DATE DEFAULT (CURRENT_DATE),
-              created_at DATE DEFAULT (CURRENT_DATE),
-              FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-          );`);
+      CREATE TABLE IF NOT EXISTS content_images (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        content_id INT NOT NULL,
+        image_url VARCHAR(255) DEFAULT NULL,
+        updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (content_id) REFERENCES contents(id) ON DELETE CASCADE
+      );
+    `);
 
     await db.query(`
-          CREATE TABLE IF NOT EXISTS roles (
-              id INT PRIMARY KEY AUTO_INCREMENT,
-              name VARCHAR(255) DEFAULT NULL UNIQUE,
-              updated DATE DEFAULT (CURRENT_DATE),
-              created_at DATE DEFAULT (CURRENT_DATE)
-          );`);
+      CREATE TABLE IF NOT EXISTS users_mappings (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        follow_id INT NOT NULL,
+        follower_id INT NOT NULL,
+        updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (follow_id) REFERENCES users(id) ON DELETE CASCADE,
+        FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE
+      );
+    `);
 
     await db.query(`
-          CREATE TABLE IF NOT EXISTS contracts (
-              id INT PRIMARY KEY AUTO_INCREMENT,
-              name VARCHAR(255) DEFAULT NULL UNIQUE,
-              updated DATE DEFAULT (CURRENT_DATE),
-              created_at DATE DEFAULT (CURRENT_DATE)
-          );`);
+      CREATE TABLE IF NOT EXISTS users_shop_mappings (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        user_id INT NOT NULL,
+        shop_id INT NOT NULL,
+        updated DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+        FOREIGN KEY (shop_id) REFERENCES shops(id) ON DELETE CASCADE
+      );
+    `);
 
     console.log("✅ Tables created successfully.");
   } catch (error) {
     console.error("❌ MySQL Error:", error);
-    throw error; // Ensure that the server does not start if database initialization fails
+    throw error;
   }
 };
